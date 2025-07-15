6bbea0c179eb031273e04c885dcd6b3e050283ce
Samyrjob <s.rjob@audencia.com>
2025-07-15 17:42:15 +0100

    step before recognition

--- diff_drive_robot/controllers/four_wheels_controller/four_wheels_controller.c
+++ diff_drive_robot/controllers/four_wheels_controller/four_wheels_controller.c
@@ -13,6 +13,7 @@
 #include <webots/robot.h>
 #include <webots/camera.h>
 #include <webots/distance_sensor.h>
+#include <webots/keyboard.h>
 
 /*
  * You may want to add macros here.
@@ -31,147 +32,125 @@ static WbDeviceTag wheels[4];
 
 
 
-static void wheel_set_speed(WbDeviceTag tag, double speed) {
-    if (speed >= 0.0) {
-        wb_motor_set_position(tag, INFINITY);
-        wb_motor_set_velocity(tag, speed);
+
+
+
+void set_wheel_speeds(double left, double right) {
+    wb_motor_set_position(wheels[0], INFINITY); // rear_right
+    wb_motor_set_position(wheels[1], INFINITY); // rear_left
+    wb_motor_set_position(wheels[2], INFINITY); // front_right
+    wb_motor_set_position(wheels[3], INFINITY); // front_left
+
+    wb_motor_set_velocity(wheels[0], 3.0);
+    wb_motor_set_velocity(wheels[1], 3.0);
+    wb_motor_set_velocity(wheels[2], right);
+    wb_motor_set_velocity(wheels[3], left);
+}
+
+
+int main(int argc, char** argv) {
+    /* necessary to initialize webots stuff */
+    wb_robot_init();
+
+    /*
+     * You should declare here WbDeviceTag variables for storing
+     * robot devices like this:
+     *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
+     *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
+     */
+
+     // all vehicles
+    wheels[0] = wb_robot_get_device("rear_right_wheel");
+    wheels[1] = wb_robot_get_device("rear_left_wheel");
+    wheels[2] = wb_robot_get_device("front_right_wheel");
+    wheels[3] = wb_robot_get_device("front_left_wheel");
+
+
+
+
+
+
+
+    // get the camera
+    WbDeviceTag camera = wb_robot_get_device("camera_front");
+
+    // get the IR sensor
+    //WbDeviceTag IRsensor = wb_robot_get_device("ir_sensor");
+
+
+    // enable the camera
+    if (camera == 0) {
+        printf("Camera not found!\n");
+        return -1;
     }
     else {
-        wb_motor_set_position(tag, -INFINITY);
-        wb_motor_set_velocity(tag, -speed);
+        printf("Camera found!\n");
+        wb_camera_enable(camera, TIME_STEP);
     }
-}
 
-
-//static void wheels_set_speed(double speed) {
-//    int i;
-//    for (i = 0; i < 4; i++)
-//        wheel_set_speed(wheels[i], speed);
-//}
-
-static void wheels_set_speed_front(double speed) {
-    int i;
-    for (i = 2; i < 4; i++)
-        wheel_set_speed(wheels[i], speed);
-}
-
-
-static void wheels_set_speed_rear(double speed) {
-    int i;
-    for (i = 0; i < 2; i++)
-        wheel_set_speed(wheels[i], speed);
-}
-
-
-int main(int argc, char **argv) {
-  /* necessary to initialize webots stuff */
-  wb_robot_init();
-
-  /*
-   * You should declare here WbDeviceTag variables for storing
-   * robot devices like this:
-   *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
-   *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
-   */
-
-   // all vehicles
-  wheels[0] = wb_robot_get_device("rear_right_wheel");
-  wheels[1] = wb_robot_get_device("rear_left_wheel");
-  wheels[2] = wb_robot_get_device("front_right_wheel");
-  wheels[3] = wb_robot_get_device("front_left_wheel");
+    //enable the keyboard 
+    wb_keyboard_enable(TIME_STEP);
 
 
 
 
 
-
-
-  // get the camera
-  WbDeviceTag camera = wb_robot_get_device("camera_front");
-
-  // get the IR sensor
-  //WbDeviceTag IRsensor = wb_robot_get_device("ir_sensor");
-
-
-  // enable the camera
-  if (camera == 0) {
-      printf("Camera not found!\n");
-      return -1;
-  }
-  else {
-      printf("Camera found!\n");
-      wb_camera_enable(camera, TIME_STEP);
-  }
-
-
-
-  // enable the ir sensor
-
-  //if (IRsensor == 0) {
-  //    printf("IR sensor not found ! \n");
-  //    return -1;
-  //}
-  //else {
-
-  //    printf("sensor found ! \n");
-  //    wb_distance_sensor_enable(IRsensor, TIME_STEP);
-  //}
-
-
-  // go forward
-  wheels_set_speed_front(3.0);
-  wheels_set_speed_rear(1.5);
-
-
-  /* main loop
-   * Perform simulation steps of TIME_STEP milliseconds
-   * and leave the loop when the simulation is over
-   */
-  while (wb_robot_step(TIME_STEP) != -1) {
-
-      //wheels_set_speed(2.0);
-
-
-      const unsigned char* image = wb_camera_get_image(camera);
-      int width = wb_camera_get_width(camera);
-      int height = wb_camera_get_height(camera);
-
-      // for (int x = 0; x < width; ++x) {
-          // for (int y = 0; y < height; ++y) {
-              // int r = wb_camera_image_get_red(image, width, x, y);
-              // int g = wb_camera_image_get_green(image, width, x, y);
-              // int b = wb_camera_image_get_blue(image, width, x, y);
-
-             // moyenne pour; niveau de gris
-              // int heat = (r + g + b) / 3;
-
-             // ici tu peux faire une LUT (table de couleurs) ou juste une d�tection de "chaleur"
-              // if (heat > 900) {
-                  // printf("hot pixel at (%d, %d)\n", x, y);
-              // }
-          // }
-      // }
-
-      wb_console_print("message\n");
-      fflush(stdout);
-    /*
-     * Read the sensors :
-     * Enter here functions to read sensor data, like:
-     *  double val = wb_distance_sensor_get_value(my_sensor);
+    /* main loop
+     * Perform simulation steps of TIME_STEP milliseconds
+     * and leave the loop when the simulation is over
      */
+    while (wb_robot_step(TIME_STEP) != -1) {
 
-    /* Process sensor data here */
+        //wheels_set_speed(2.0);
 
-    /*
-     * Enter here functions to send actuator commands, like:
-     * wb_motor_set_position(my_actuator, 10.0);
-     */
-  };
 
-  /* Enter your cleanup code here */
+        const unsigned char* image = wb_camera_get_image(camera);
+        int width = wb_camera_get_width(camera);
+        int height = wb_camera_get_height(camera);
 
-  /* This is necessary to cleanup webots resources */
-  wb_robot_cleanup();
+        int key = wb_keyboard_get_key();
 
-  return 0;
+        switch (key) {
+        case WB_KEYBOARD_UP:
+            set_wheel_speeds(3.0, 3.0); // forward
+            break;
+        case WB_KEYBOARD_DOWN:
+            set_wheel_speeds(-3.0, -3.0); // backward
+            break;
+        case WB_KEYBOARD_LEFT:
+            set_wheel_speeds(-2.0, 2.0); // turn left
+            break;
+        case WB_KEYBOARD_RIGHT:
+            set_wheel_speeds(2.0, -2.0); // turn right
+            break;
+        case -1:
+            set_wheel_speeds(0.0, 0.0); // stop
+            break;
+
+
+
+            wb_console_print("message\n");
+            fflush(stdout);
+            /*
+             * Read the sensors :
+             * Enter here functions to read sensor data, like:
+             *  double val = wb_distance_sensor_get_value(my_sensor);
+             */
+
+             /* Process sensor data here */
+
+             /*
+              * Enter here functions to send actuator commands, like:
+              * wb_motor_set_position(my_actuator, 10.0);
+              */
+        };
+
+    }
+
+        /* Enter your cleanup code here */
+
+        /* This is necessary to cleanup webots resources */
+        wb_robot_cleanup();
+
+        return 0;
 }
